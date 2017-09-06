d7d7fe69b093d2171984de8573a2f47727006626
Lars Hegnes Sendstad <Lars.Sendstad@nhh.no>
2017-09-06 19:20:44 +0200

    Add existing file

--- js/3dGraph.js
+++ js/3dGraph.js
@@ -35,7 +35,23 @@ function fun_3dPlot() {
         }
         z3.push(z3_row);
     }
-    var data_z1 = {z: value_regime1_analytical,
+
+    function matrixLoop(varName,idx) {
+        var outputMatrix =[]
+        varName.forEach(function(entry1) {
+            var tempMatrix =[];
+            entry1.forEach(function(entry2) {
+                tempMatrix.push(entry2[idx]);
+            });
+            outputMatrix.push(tempMatrix);
+        });
+        return outputMatrix
+    };
+
+
+    var data_z1 = {z: matrixLoop(value_regime1_analytical,5),
+                             y:sigma1_3d,
+                             x:sigma2_3d,
                              type: 'surface'};
     var data_z2 = {z: z2, showscale: false, opacity:0.9, type: 'surface'};
     var data_z3 = {z: z3, showscale: false, opacity:0.9, type: 'surface'};
