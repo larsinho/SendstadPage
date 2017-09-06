a94e6f857d44d77984005e8ae5dba6d66fcbcf03
Lars Hegnes Sendstad <Lars.Sendstad@nhh.no>
2017-09-06 18:24:45 +0200

    Add existing file

--- js/Plotting.js
+++ js/Plotting.js
@@ -171,7 +171,7 @@ function fun_main() {
 };
 
 
-var promise1 = $.getJSON("/SendstadPage/SubPlottingScripts/value_regime1_analytical.json", function(json) {
+var promise1 = $.getJSON("//SubPlottingScripts/value_regime1_analytical.json", function(json) {
     value_regime1_analytical = json;
 });
 var promise2 = $.getJSON("/SendstadPage/SubPlottingScripts/value_regime1_numerical.json", function(json) {
